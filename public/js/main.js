6f8dfb3182829b5a02b45e12992e7de46e58ca9c
Stephan Griesel <sgriesel@gmail.com>
2017-10-08 19:58:56 +0200

    spritesheet and draw working

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+function loadImage(url) {
+    return new Promise(resolve => {
+        const image = new Image();
+        image.addEventListener('load', () => {
+            resolve(image);
+        });
+        image.src = url;
+    });
+}
+
+const canvas = document.getElementById('screen');
+const context = canvas.getContext('2d');
+
+context.fillRect(0,0,50,50);
+
+loadImage('/img/tiles.png')
+.then(image => {
+    const sprites = new SpriteSheet(image, 16, 16);
+    sprites.define('ground', 0, 0);
+    sprites.draw('ground', context, 45, 62);
+});
